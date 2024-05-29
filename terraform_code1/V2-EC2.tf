871e20a85faf0a5088ba57f1aa25d8a621f92ba3
Maddula <jyosna.maddula@capgemini.com>
2024-05-29 22:33:33 +0530

    added files

---
+++ terraform_code1/V2-EC2.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "us-east-1"
+}
+resource "aws_instance" "demo-server"{
+    ami = "ami-0bb84b8ffd87024d8"
+    instance_type = "t2.micro"
+    key_name = "Mykeypair"
+    security_groups=["demo-sg"]
+}
+resource "aws_security_group" "demo-sg"{
+    name = "demo-sg"
+    description ="SSH Access"
+    ingress {
+        description ="shh access"
+        from_port =22
+        to_port=22
+        protocol="tcp"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    egress{
+        from_port=0
+        to_port=0
+        protocol="-1"
+        cidr_blocks=["0.0.0.0/0"]
+    }
+    tags={
+        Name="ssh-prot"
+    }
+}
